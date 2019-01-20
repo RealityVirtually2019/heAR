83d220254aeb970785d07580f3c711592e4c6ff4
Richard Gao <rgao@olin.edu>
2019-01-20 13:10:36 -0500

    Made bigger

---
+++ heAR/Assets/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public float delay = 440;
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(1);
+    }
+}
